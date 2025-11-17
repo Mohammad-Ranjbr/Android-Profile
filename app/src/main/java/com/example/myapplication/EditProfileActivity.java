dc0970920dddfd118281a150b9887440c29c650c
Mohammad-Ranjbr <mohammadranjbar.mmr81@gmail.com>
2025-11-17 09:01:30 +0330

    use lambda

--- app/src/main/java/com/example/myapplication/EditProfileActivity.java
+++ app/src/main/java/com/example/myapplication/EditProfileActivity.java
@@ -3,7 +3,6 @@ package com.example.myapplication;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -23,17 +22,13 @@ public class EditProfileActivity extends AppCompatActivity {
         EditText editText = findViewById(R.id.et_editProfile);
         editText.setText(fullName);
         Button doneButton = findViewById(R.id.btn_editProfile_done);
-        doneButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String fullName = editText.getText().toString();
-                Intent intent = new Intent();
-                intent.putExtra(MainActivity.EXTRA_KEY_FULLNAME, fullName);
-                setResult(Activity.RESULT_OK, intent);
-                finish();
-            }
+        doneButton.setOnClickListener(v -> {
+            String editedFullName = editText.getText().toString();
+            Intent intent = new Intent();
+            intent.putExtra(MainActivity.EXTRA_KEY_FULLNAME, editedFullName);
+            setResult(Activity.RESULT_OK, intent);
+            finish();
         });
-
-
     }
+
 }
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,15 +4,12 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
-import android.widget.CompoundButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -26,86 +23,61 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button saveInformationButton = findViewById(R.id.btn_main_saveInformation);
-        saveInformationButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "User clicked on save information button", Toast.LENGTH_SHORT).show();
-            }
-        });
+        saveInformationButton.setOnClickListener(v ->
+                Toast.makeText(MainActivity.this, "User clicked on save information button", Toast.LENGTH_SHORT).show());
 
         CheckBox androidSkillCheckBox = findViewById(R.id.checkBox_main_android);
-        androidSkillCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(@NonNull CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    Toast.makeText(MainActivity.this, "Android skill is checked", Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(MainActivity.this, "Android skill is not checked", Toast.LENGTH_SHORT).show();
-                }
+        androidSkillCheckBox.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+                Toast.makeText(MainActivity.this, "Android skill is checked", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(MainActivity.this, "Android skill is not checked", Toast.LENGTH_SHORT).show();
             }
         });
 
         CheckBox deepLearningSkillCheckBox = findViewById(R.id.checkBox_main_deepLearning);
-        deepLearningSkillCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(@NonNull CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    Toast.makeText(MainActivity.this, "Deep Learning is checked", Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(MainActivity.this, "Deep Learning is not checked", Toast.LENGTH_SHORT).show();
-                }
+        deepLearningSkillCheckBox.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+                Toast.makeText(MainActivity.this, "Deep Learning is checked", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(MainActivity.this, "Deep Learning is not checked", Toast.LENGTH_SHORT).show();
             }
         });
 
         CheckBox uiDesignSkillCheckBox = findViewById(R.id.checkBox_main_uiDesign);
-        uiDesignSkillCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(@NonNull CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    Toast.makeText(MainActivity.this, "Ui Design is checked", Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(MainActivity.this, "Ui Design is not checked", Toast.LENGTH_SHORT).show();
-                }
+        uiDesignSkillCheckBox.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+                Toast.makeText(MainActivity.this, "Ui Design is checked", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(MainActivity.this, "Ui Design is not checked", Toast.LENGTH_SHORT).show();
             }
         });
 
         RadioGroup radioGroup = findViewById(R.id.radioGroup_main);
-        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(@NonNull RadioGroup group, int checkedId) {
-
-                if (checkedId == R.id.radioButton_main_Tehran) {
-                    Toast.makeText(MainActivity.this, "Tehran RadioButton is selected", Toast.LENGTH_SHORT).show();
-                }
-                else if (checkedId == R.id.radioButton_main_Alborz) {
-                    Toast.makeText(MainActivity.this, "Alborz RadioButton is selected", Toast.LENGTH_SHORT).show();
-                }
-                else if (checkedId == R.id.radioButton_main_Gilan) {
-                    Toast.makeText(MainActivity.this, "Gilan RadioButton is selected", Toast.LENGTH_SHORT).show();
-                }
+        radioGroup.setOnCheckedChangeListener((group, checkedId) -> {
+            if (checkedId == R.id.radioButton_main_Tehran) {
+                Toast.makeText(MainActivity.this, "Tehran RadioButton is selected", Toast.LENGTH_SHORT).show();
+            } else if (checkedId == R.id.radioButton_main_Alborz) {
+                Toast.makeText(MainActivity.this, "Alborz RadioButton is selected", Toast.LENGTH_SHORT).show();
+            } else if (checkedId == R.id.radioButton_main_Gilan) {
+                Toast.makeText(MainActivity.this, "Gilan RadioButton is selected", Toast.LENGTH_SHORT).show();
             }
         });
 
         Button editProfileButton = findViewById(R.id.btn_main_editProfile);
-        editProfileButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, EditProfileActivity.class); // Explicit Intent -> Specific destination
-                //startActivity(intent);
-                TextView textView = findViewById(R.id.tv_main_fullName);
-                intent.putExtra(EXTRA_KEY_FULLNAME, textView.getText());
-                startActivityForResult(intent, 1001);
-            }
+        editProfileButton.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, EditProfileActivity.class); // Explicit Intent -> Specific destination
+            //startActivity(intent);
+            TextView textView = findViewById(R.id.tv_main_fullName);
+            intent.putExtra(EXTRA_KEY_FULLNAME, textView.getText());
+            startActivityForResult(intent, 1001);
         });
 
-         Button viewWebsiteButton = findViewById(R.id.btn_main_viewWebSite);
-         viewWebsiteButton.setOnClickListener(new View.OnClickListener() {
-             @Override
-             public void onClick(View v) {
-                 Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/Mohammad-Ranjbr"));
-                 startActivity(intent);
-             }
-         });
+        Button viewWebsiteButton = findViewById(R.id.btn_main_viewWebSite);
+        viewWebsiteButton.setOnClickListener(v -> {
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/Mohammad-Ranjbr"));
+            startActivity(intent);
+        });
 
     }
 
